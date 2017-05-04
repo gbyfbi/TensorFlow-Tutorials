b566c193016a25abf4d22d262740f7d64cbe6e41
GBY <gaoboyang@gmail.com>
2017-05-04 18:53:43 +0200

    forgot what this is.

--- visualize_graph.py
+++ visualize_graph.py
@@ -3,12 +3,14 @@ import os.path
 import tensorflow as tf
 from tensorflow.python.platform import gfile
 
-INCEPTION_LOG_DIR = '/tmp/inception_v3_log'
+# INCEPTION_LOG_DIR = '/tmp/inception_v3_log'
+INCEPTION_LOG_DIR = '/tmp/vgg_16'
 
 if not os.path.exists(INCEPTION_LOG_DIR):
     os.makedirs(INCEPTION_LOG_DIR)
 with tf.Session() as sess:
-    model_filename = '/home/gao/Dropbox/Deeplearning/tensorflow/models/imagenet/inception-2015-12-05/classify_image_graph_def.pb'
+    # model_filename = '/home/gao/Dropbox/Deeplearning/tensorflow/models/imagenet/inception-2015-12-05/classify_image_graph_def.pb'
+    model_filename = '/home/gao/Data/flower/tf_data/slim/flower_fine_tune/checkpoints/vgg_16.ckpt'
     with gfile.FastGFile(model_filename, 'rb') as f:
         graph_def = tf.GraphDef()
         graph_def.ParseFromString(f.read())
